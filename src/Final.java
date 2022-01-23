8ff4b2574b79a91864c4c0578385cdb395de993a
alxmart <alxmartinez@gmail.com>
2022-01-23 13:09:29 -0300

    Adding more exercises

---
+++ src/Final.java
@@ -0,0 +1,12 @@
+public class Final {
+	public static void main(String[] args) { 
+
+    final double pi = 3.14;
+
+    //pi = 77.89; (Cannot change its value)
+
+    System.out.println(pi);
+      
+		
+	}
+}
